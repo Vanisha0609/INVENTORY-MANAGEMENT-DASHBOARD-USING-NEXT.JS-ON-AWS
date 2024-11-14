392d2f1995e81eb3ebdc76ade973fbce0362920c
vanisha.ramesh <119104009+Vanisha0609@users.noreply.github.com>
2024-11-15 00:13:31 +0530

    Update ecosystem.config.js

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,10 +1,13 @@
-module.exports={
-    apps: [
-        {
-            name: "inventory-management",
-            script: "npm",
-            args: "run dev",
-            env:{
-                NODE_ENV: "development",
-                ENV_VAR1: "environment-variable",
-            }
+module.exports = {
+  apps: [
+    {
+      name: "inventory-management",
+      script: "npm",
+      args: "run dev",
+      env: {
+        NODE_ENV: "development",
+        ENV_VAR1: "environment-variable",
+      },
+    },
+  ],
+};
