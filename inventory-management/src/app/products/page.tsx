ca705a6b107f5d00a1ab26ac8d85d70de95b74d9
vanisha.ramesh <119104009+Vanisha0609@users.noreply.github.com>
2024-11-15 11:03:36 +0530

    Update page.tsx

--- inventory-management/src/app/products/page.tsx
+++ inventory-management/src/app/products/page.tsx
@@ -81,7 +81,7 @@ const Products = () => {
             >
               <div className="flex flex-col items-center">
                 <Image
-                  src={`https://s3-inventorymanagement.s3.ap-south-1.amazonaws.com/product${
+                  src={`https://s3-inventorymanagement.s3.ap-south-1a.amazonaws.com/product${
                     Math.floor(Math.random() * 3) + 1
                   }.png`}
                   alt={product.name}
